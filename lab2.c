2e69700353fa4d3010836ab96f1640000ff6bdbb
Milita Andrei <quinnlovelost@gmail.com>
2026-03-03 12:47:43 +0200

    Fix: functia recursiva nu avea conditie de oprire si rula la infinit

--- lab2.c
+++ lab2.c
@@ -1,17 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+int function (int x)
+{
+	if (x <= 0)
+		return x;
+	x = x-2;
+	return function(x);
 }
 
-int main(){
-	int a=30, i=0;
-	
-	while (a>0){
-		a=function(a);
+int main ()
+{
+	int a = 30, i = 0;
+	while (a > 0)
+	{
+		a = function(a);
 		i++;
 	}
 	printf("%d", i);
